9c4386d5dc3571c8bdf978c37bbf7e8d2585092d
Jonng <jonngkh@gmail.com>
2024-02-25 23:20:52 +0800

    laser sort of working. had to convert many things from the 3.1 tutorial for 4.2

--- Laser.gd
+++ Laser.gd
@@ -1,55 +1,82 @@
 extends RayCast2D
 
-var is_casting = false
-var tween = create_tween()
+# Speed at which the laser extends when first fired, in pixels per seconds.
+@export var cast_speed := 7000.0
+# Maximum length of the laser in pixels.
+@export var max_length := 1400.0
+# Base duration of the tween animation in seconds.
+@export var growth_time := 0.1
+
+var is_casting := false: set = set_is_casting
+
+@onready var fill := $Line2D
+#@onready var tween = create_tween()
+@onready var firing_particles = $FiringParticles2D
+@onready var collision_particles = $CollisionParticles2D
+#@onready var beam_particles = $BeamParticles2D
+@onready var line_width: float = fill.width
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(false)
-	$Line2D.points[1] = Vector2.ZERO
+	fill.points[1] = Vector2.ZERO
+	fill.width = 1.0
 	
-func _input(event):
-	if event is InputEventMouseButton:
-		is_casting = event.pressed
-		print(is_casting)
-		#is_casting = event.pressed
-
-
-func _process_process(delta):
-	var cast_point
+func _physics_process(delta):
+	target_position = (target_position + Vector2.RIGHT * cast_speed * delta).limit_length(max_length)
+	cast_beam()
+	print(is_casting)
+	
+func cast_beam():
+	var cast_point := target_position
 	force_raycast_update()
 	
-	$CollisionParticles2D.emitting = is_colliding()
+	collision_particles.emitting = is_colliding()
 	
 	if is_colliding():
 		cast_point = to_local(get_collision_point())
-		$CollisionParticles2D.global_rotation = get_collision_normal().angle()
-		$CollisionParticles2D.position = cast_point
+		collision_particles.global_rotation = get_collision_normal().angle()
+		collision_particles.position = cast_point
 		
-	$Line2D.points[1] = cast_point
+	fill.points[1] = cast_point
+	#beam_particles.position = cast_point * 0.5
+	#beam_particles.process_material.emission_box_extents.x = cast_point.length() * 0.5
 	
-	
+#func _input(event):
+	#if event is InputEventMouseButton:
+		#is_casting = event.pressed
+		#print(is_casting)
+		##is_casting = event.pressed
+
+
 func set_is_casting(cast: bool):
 	is_casting = cast
 	
 	$FiringParticles2D.emitting = is_casting
 	if is_casting:
+		target_position = Vector2.ZERO
+		fill.points[1] = target_position
 		appear()
 	else:
-		$CollisionParticles2D.emitting = false
+		fill.points[1] = Vector2.ZERO
+		collision_particles.emitting = false
 		disappear()
 	
 	set_physics_process(is_casting)
+	firing_particles.emitting = is_casting
 	
 
 func appear():
+	var tween = create_tween()
 	if tween.is_running():
 		tween.stop()
-	tween.tween_property($Line2D, "width", 10.0, 0.2).from(0.0)
-	tween.start()
+	tween.tween_property(fill, "width", line_width, growth_time * 2).from(0.0)
+	tween.play()
 	
 func disappear():
+	var tween = create_tween()
 	if tween.is_running():
 		tween.stop()
-	tween.tween_property($Line2D, "width", 0, 0.1).from(10.0)
-	tween.start()
+	tween.tween_property(fill, "width", 0.0, growth_time).from(fill.width)
+	tween.play()
