5c6811d8e42700ede5a02f7c8322da8a35219900
Jonng <jonngkh@gmail.com>
2024-03-12 07:31:03 +0800

    spell goblin has a progress bar to visualise channeling progress

--- Enemy/SpellGoblin States/Charge.gd
+++ Enemy/SpellGoblin States/Charge.gd
@@ -2,8 +2,10 @@
 extends SpellGoblinState
 
 @onready var charge_timer = $"../../ChargeTimer"
+@onready var charge_progress_bar = $"../../ChargeProgressBar"
 
-@export var charge_duration = 1.5
+
+@export var charge_duration = 1.0
 
 # Called by the state machine upon changing the active state. The `msg` parameter is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(_msg := {}) -> void:
@@ -16,6 +18,7 @@ func _on_animated_sprite_2d_animation_finished():
 	if spellgoblin.animated_sprite.animation == "spellgoblin_chargestart":
 		spellgoblin.animated_sprite.play("spellgoblin_charge")
 		charge_timer.start()
+		charge_progress_bar.visible = true
 
 
 func _on_charge_timer_timeout():
@@ -26,8 +29,8 @@ func _on_charge_timer_timeout():
 
 # Corresponds to the `_process()` callback.
 func update(_delta: float) -> void:
-	#spellgoblin.animated_sprite.play("spellgoblin_idle")
-	pass
+	charge_progress_bar.value = 100 - (charge_timer.time_left * 100 / charge_timer.wait_time)
+
 
 # Corresponds to the `_physics_process()` callback.
 func physics_update(_delta: float) -> void:
@@ -36,7 +39,7 @@ func physics_update(_delta: float) -> void:
 
 # Called by the state machine before changing the active state. Use this function to clean up the state.
 func exit() -> void:
-	pass
+	charge_progress_bar.visible = false
 
 
 # Receives events from the `_unhandled_input()` callback.
