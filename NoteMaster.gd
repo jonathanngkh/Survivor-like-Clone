d1bb6f4749be33de8ed1cc413011da4684353967
Jonng <jonngkh@gmail.com>
2024-04-15 22:38:24 +0800

    DO graphic appears when Do is played

--- NoteMaster.gd
+++ NoteMaster.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
+@onready var player = get_tree().get_first_node_in_group("player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite2D.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	position = player.global_position
 	pass
 
 func _unhandled_input(event):
 	if Input.is_key_pressed(KEY_1):
-		$C4.play()
+		$C5.play()
+		$AnimatedSprite2D.visible = true
+		sprite_fade()
+		
+func sprite_fade() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($AnimatedSprite2D, "modulate:a", 0, 0.6).from(1.0).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_EXPO)
+	tween.play()
